aab6ed9996c903cedd852773ee2ab7bdc8a13551
Hollow Man <hollowman@hollowman.ml>
2021-01-27 18:22:01 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- K8s/create.sh
+++ K8s/create.sh
@@ -2,8 +2,42 @@
 
 # Setup Secrets
 echo "---Enter your Actions Secrets, empty if it doesn't exists---"
-secrets=("title" "msg" "serverchansckey" "openid" "pptoken" "pptopic")
 command="kubectl create secret generic wechat-message-secret"
+
+if [ -f "tmpt" ]; then
+    rm tmpt
+fi
+if [ -f "tmpm" ]; then
+    rm tmpm
+fi
+echo "-title(Empty line to end):"
+while read content
+do
+    if [ -z "$content" ]
+    then
+        break
+    else
+        echo "$content" >> tmpt
+    fi
+done
+if [ -f "tmpt" ]; then
+    command=$command" --from-file=title=tmpt"
+fi
+echo "-msg(Empty line to end):"
+while read content
+do
+    if [ -z "$content" ]
+    then
+        break
+    else
+        echo "$content" >> tmpm
+    fi
+done
+if [ -f "tmpm" ]; then
+    command=$command" --from-file=msg=tmpm"
+fi
+
+secrets=("serverchansckey" "openid" "pptoken" "pptopic")
 for secret in ${secrets[*]}
 do
     read -p "-"$secret": " content
